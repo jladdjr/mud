cb98c8652b3439ec6ff69724deb0d2486da69718
Jim Ladd <jladdjr@gmail.com>
2022-09-26 21:03:25 -0700

    test StorageController.get_machine

--- tests/test_mud.py
+++ tests/test_mud.py
@@ -7,12 +7,20 @@ from storage import StorageController
 
 class StorageControllerTests(unittest.TestCase):
 
-
     @mock.patch('storage.connect')
     def test_add_machine(self, connect_mock):
         sc = StorageController()
         sc.add_machine('foo_machine', 'this is the foo machine')
         expected_execute_args = ('INSERT INTO machines (hostname, description) VALUES (%s, %s)',
                                  ('foo_machine', 'this is the foo machine'))
+        execute_mock = connect_mock.return_value.cursor.return_value.execute
+        execute_mock.assert_called_once_with(*expected_execute_args)
+
+    @mock.patch('storage.connect')
+    def test_get_machine(self, connect_mock):
+        sc = StorageController()
+        sc.get_machine('foo_machine')
+        expected_execute_args = ('SELECT hostname, description from machines where hostname = %s',
+                                 ('foo_machine',))
         execute_mock = connect_mock.return_value.cursor.return_value.execute
         execute_mock.assert_called_once_with(*expected_execute_args)
